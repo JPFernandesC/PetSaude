a2c51a137e08b1bd88f907f6579044465f21bf43
JPFernandesC <jpfalves11@gmail.com>
2025-06-13 17:22:35 -0300

    .

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native"
+import { NavigationContainer } from "@react-navigation/stack"
 import AppNavigator from './src/AppNavigator';
 
 export default function App() {
